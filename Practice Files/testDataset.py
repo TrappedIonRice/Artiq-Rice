623513c3bcede6cba0bf6606586b5659ef544bf1
Abster-snk <67212104+Abster-snk@users.noreply.github.com>
2023-08-10 21:32:00 -0500

    Delete Practice Files directory

--- Practice Files/testDataset.py
+++
@@ -1,9 +0,0 @@
-from ndscan.experiment import *
-
-class TestDataset(EnvExperiment):
-    def run(self):
-        print(self.get_dataset("temp_dataset"))
-        num = 5*s
-        print(num)
-        num = 5*MHz
-        print(num)
