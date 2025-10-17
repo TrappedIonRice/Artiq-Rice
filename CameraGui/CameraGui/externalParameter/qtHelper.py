9eb10ea5ab7ff536a3a9730b44db47271c6c86c6
BRK00B025JTRQ54\TrappedIonRiceXPS <gt21@rice.edu>
2025-10-17 12:07:59 -0500

    removing Lucius camera gui folder to avoid conflicts while merging branches

--- CameraGui/CameraGui/externalParameter/qtHelper.py
+++
@@ -1,6 +0,0 @@
-from PyQt5 import QtCore
-
-class qtHelper(QtCore.QObject):
-    newData = QtCore.pyqtSignal(object, object)
-    def __init__(self):
-        super(qtHelper, self).__init__()
--- CameraGui/CameraGui/modules/filetype.py
+++
@@ -1,7 +0,0 @@
-from lxml import etree
-
-def isXmlFile(filename):
-    with open(filename) as f:
-        first_line = f.readline()
-    return first_line.strip().startswith("<?xml")
-
